4e80af4386b08614f74b45283c58e3eaa8102f14
telaprolulasya <2300031910cseh@gmail.com>
2025-11-04 21:30:38 +0530

    Change CART_API_URL to localhost
    Updated CART_API_URL to use localhost for local development.

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Use the correct base URL for cart operations
-const CART_API_URL = "http://ec2-35-88-110-92.us-west-2.compute.amazonaws.com:30083/back1/api/cart";
+const CART_API_URL = "http://localhost:30083/back1/api/cart";
 
 export const addToCart = async (userId, productId) => {
   return await axios.post(`${CART_API_URL}/add`, {
